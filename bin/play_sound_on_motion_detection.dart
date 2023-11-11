ead29b5f1eed0bee6fc8a92f7da923a392f823b8
Nick Meinhold <nick@enspyr.co>
2023-11-11 19:39:23 +1100

    Initial commit

---
+++ bin/play_sound_on_motion_detection.dart
@@ -0,0 +1,5 @@
+import 'package:play_sound_on_motion_detection/play_sound_on_motion_detection.dart' as play_sound_on_motion_detection;
+
+void main(List<String> arguments) {
+  print('Hello world: ${play_sound_on_motion_detection.calculate()}!');
+}
---
+++ test/play_sound_on_motion_detection_test.dart
@@ -0,0 +1,8 @@
+import 'package:play_sound_on_motion_detection/play_sound_on_motion_detection.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
